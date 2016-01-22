e926374bfaa1b5cdb519333732fd4a30857a35af
gil <gilovi@users.noreply.github.com>
2016-01-22 09:45:18 +0200

    on the way there

--- getImage.m
+++ getImage.m
@@ -4,25 +4,30 @@ function [image] = getImage(assignmentPositionsX,assignmentPositionsY,samplingPo
 % pyrmamid, and assignment indices in the highres image return a high resolution image
 %
 % Arguments:
-% assignmentPositionsX ? (m ? 4) ª (n ? 4) ª 3 ª 5 ª 5 x assignment coordinates in the high resolution image (
+% assignmentPositionsX ? (m ? 4) ï¿½ (n ? 4) ï¿½ 3 ï¿½ 5 ï¿½ 5 x assignment coordinates in the high resolution image (
 % getSamplingInformation output)
-% assignmentPositionsY ? (m ? 4) ª (n ? 4) ª 3 ª 5 ª 5 y assignment coordinates in the high resolution image (
+% assignmentPositionsY ? (m ? 4) ï¿½ (n ? 4) ï¿½ 3 ï¿½ 5 ï¿½ 5 y assignment coordinates in the high resolution image (
 % getSamplingInformation output)
-% samplingPositionsX ? (m ? 4) ª (n ? 4) ª 3 ª 5 ª 5 x sampling coordinates in the rendered pyramid image (
+% samplingPositionsX ? (m ? 4) ï¿½ (n ? 4) ï¿½ 3 ï¿½ 5 ï¿½ 5 x sampling coordinates in the rendered pyramid image (
 % getSamplingInformation output)
-% samplingPositionsY ? (m ? 4) ª (n ? 4) ª 3 ª 5 ª 5 y sampling coordinates in the rendered pyramid image (
+% samplingPositionsY ? (m ? 4) ï¿½ (n ? 4) ï¿½ 3 ï¿½ 5 ï¿½ 5 y sampling coordinates in the rendered pyramid image (
 % getSamplingInformation output)
-% weights ? (m ? 4) ª (n ? 4) ª 3 matrix with the weights for each DB candidate
-% emptyHighResImage ? M ª N zeros image, where M and N are the dimensions of a level in the pyramid that should
+% weights ? (m ? 4) ï¿½ (n ? 4) ï¿½ 3 matrix with the weights for each DB candidate
+% emptyHighResImage ? M ï¿½ N zeros image, where M and N are the dimensions of a level in the pyramid that should
 % be reconstructed in this function
 % renderedPyramid ? a single image containing all levels of the pyramid
 %
 % Outputs:
-% image ? M ª N high resolution image
+% image ? M ï¿½ N high resolution image
 %
 I = repmat(emptyHighResImage,[1,1,3,25]);
-for i = 1:25
-    [xp,yp]=ind2sub(i);
-    I(:,:,:,i) = interp2(samplingPositionsX(xp:5:end), samplingPositionsY(yp:5:end),...
-        renderedPyramid, assignmentPositionsX, assignmentPositionsY);
+for i=1:3
+    for j = 1:25
+        [xp,yp] = ind2sub([5,5], i);
+        I(:,:,i,j) = interp2(samplingPositionsX(:, :, i, xp:5:end,yp:5:end), samplingPositionsY(:, :, i, xp:5:end,yp:5:end),...
+        renderedPyramid, assignmentPositionsX(:, :, i, xp:5:end,yp:5:end), assignmentPositionsY(:, :, i, xp:5:end,yp:5:end)...
+       ,'cubic');
+    end
 end
+
+imshow(I(:,:,1,1))
